1b292258456c7d3a2fb95f12a0b483e10901fbc1
Nadhir <nadhirxz@gmail.com>
2021-11-29 01:06:48 +0100

    Generate torrent file

--- utils/torrent.js
+++ utils/torrent.js
@@ -1,14 +1,17 @@
 const parseTorrent = require('parse-torrent');
+const { writeFileSync } = require('fs');
 const { torrentDiscovery } = require('./discovery');
 
 function getTorrent(magnet, output) {
 	try {
-		magnet = parseTorrent(
-			'magnet:?xt=urn:btih:617f00090168614fb27a73e1ebba2a15f8980d71&dn=%5BJudas%5D%20Mushoku%20Tensei%20-%20S01E20%20%5B1080p%5D%5BHEVC%20x265%2010bit%5D%5BMulti-Subs%5D%20%28Weekly%29&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce'
-		);
-		output = output || magnet.infoHash;
+		magnet = parseTorrent(magnet);
 		torrentDiscovery(magnet).then(metadata => {
-			// console.log(metadata);
+			metadata = parseTorrent(metadata);
+			output = output || metadata.name || magnet.infoHash.toUpperCase();
+			const file = parseTorrent.toTorrentFile({ ...parseTorrent(metadata), announce: magnet.announce || [] });
+			const filename = (/[.]/.exec(output) ? /[^.]+$/.exec(output)[0] : undefined) == 'torrent' ? output : output + '.torrent';
+			writeFileSync(filename, file);
+			process.exit(0);
 		});
 	} catch (error) {
 		console.log(error.message);
