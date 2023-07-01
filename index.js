011fa7f2feef9ed8e4b28d5be8ea56d7a52a98c4
Nadhir Ferdji <nadhirxz@gmail.com>
2023-07-01 22:13:50 +0100

    cli: added timeout option

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const { program } = require('commander');
 const { getTorrent } = require('./utils/cli');
+const config = require('./config');
 require('pkginfo')(module);
 
 program
@@ -8,5 +9,6 @@ program
 	.description(module.exports.description)
 	.argument('<magnet>', 'full magnet uri or info hash only')
 	.option('-o, --output <filename>', 'torrent file output')
+	.option('-t, --timeout <timeout>', 'timeout in ms', val => parseInt(val) || config.TIMEOUT, config.TIMEOUT)
 	.action(async magnet => await getTorrent(magnet, program.opts()))
 	.parse();
